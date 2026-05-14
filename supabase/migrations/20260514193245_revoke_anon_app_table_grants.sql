3b034c5d842132e21a62f5a2cd4c1698c0b3517e
Jride-Dev <Jride-Dev@users.noreply.github.com>
2026-05-14 12:37:54 -0700

    Revoke anon app table grants

---
+++ supabase/migrations/20260514193245_revoke_anon_app_table_grants.sql
@@ -0,0 +1,27 @@
+revoke all on
+  customer_messages,
+  customers,
+  job_daily_sequences,
+  job_events,
+  job_images,
+  job_parts,
+  job_services,
+  jobs,
+  shop_members,
+  transaction_number_sequences,
+  work_logs
+from anon;
+
+revoke all on
+  customer_messages,
+  customers,
+  job_daily_sequences,
+  job_events,
+  job_images,
+  job_parts,
+  job_services,
+  jobs,
+  shop_members,
+  transaction_number_sequences,
+  work_logs
+from public;
